b7229276c7360ec6ce1317bdebd57f06a3b6a5fa
a <102840087+letter-aa@users.noreply.github.com>
2023-08-21 14:22:56 -0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-LSTATUS RegViewKeyData(HKEY hKey, string key, LPCWSTR lpSubKey, TCHAR * &result) {
+LSTATUS RegViewKeyData(HKEY hKey, string key, LPCWSTR lpSubKey, TCHAR*& result) {
     HKEY phkey;
     LONG regKeyParent = RegOpenKeyEx(hKey, lpSubKey, 0, KEY_READ, &phkey);
     if (regKeyParent == ERROR_SUCCESS) {
@@ -11,10 +11,15 @@ LSTATUS RegViewKeyData(HKEY hKey, string key, LPCWSTR lpSubKey, TCHAR * &result)
         TCHAR lpb[255];
         DWORD lpd = sizeof(lpb);
         LONG regKey = RegQueryValueEx(phkey, wideKey.c_str(), NULL, NULL, (LPBYTE)lpb, &lpd);
-        result = new TCHAR[lpd];
-        memcpy(result, lpb, lpd);
-        RegCloseKey(phkey);
-        return 0 && ERROR_SUCCESS;
+        if (regKey == ERROR_SUCCESS) {
+            result = new TCHAR[lpd];
+            memcpy(result, lpb, lpd);
+            RegCloseKey(phkey);
+            return 0 && ERROR_SUCCESS;
+        }
+        else {
+            return 1 && ERROR_FILE_NOT_FOUND;
+        }
     }
     else {
         //ERROR_FILE_NOT_FOUND
@@ -23,25 +28,30 @@ LSTATUS RegViewKeyData(HKEY hKey, string key, LPCWSTR lpSubKey, TCHAR * &result)
 }
 int main()
 {
-    TCHAR * result;
-    int ok = RegViewKeyData(HKEY_CURRENT_USER,"ProgID",L"Software\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\https\\UserChoice", result);
-    wstring preRes;
-    //string * resultConv = reinterpret_cast<string *>(result);
-    if (wcscmp(result, L"MSEdgeHTM") == 0) {
-        preRes = L"Microsoft Edge";
-    }
-    else if (wcscmp(result, L"ChromeHTML") == 0) {
-        preRes = L"Google Chrome";
-    }
-    else if (wcscmp(result, L"FirefoxHTML") == 0) {
-        preRes = L"Firefox";
-    }
-    else if (wcscmp(wstring(result).substr(0, 8).c_str(), L"Opera GX") == 0) {
-        preRes = L"Opera GX";
+    TCHAR* result;
+    int ok = RegViewKeyData(HKEY_CURRENT_USER, "ProgID", L"Software\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\https\\UserChoice", result);
+    if (ok == ERROR_SUCCESS) {
+        wstring preRes;
+        //string * resultConv = reinterpret_cast<string *>(result);
+        if (wcscmp(result, L"MSEdgeHTM") == 0) {
+            preRes = L"Microsoft Edge";
+        }
+        else if (wcscmp(result, L"ChromeHTML") == 0) {
+            preRes = L"Google Chrome";
+        }
+        else if (wcscmp(result, L"FirefoxHTML") == 0) {
+            preRes = L"Firefox";
+        }
+        else if (wcscmp(wstring(result).substr(0, 8).c_str(), L"Opera GX") == 0) {
+            preRes = L"Opera GX";
+        }
+        else {
+            preRes = L"idk lol";
+        }
+        wstring result2 = L"Your default browser is " + preRes;
+        MessageBox(NULL, result2.c_str(), TEXT("the default browser checker"), NULL);
     }
     else {
-        preRes = L"idk lol";
-    }    
-    wstring result2 = L"Your default browser is " + preRes;
-    MessageBox(NULL, result2.c_str(), TEXT("the default browser checker"), NULL);
+        MessageBox(NULL, TEXT("error: registry key does not exist\n\n haha skill issue"), TEXT("the default browser checker"), NULL);
+    }
 }
